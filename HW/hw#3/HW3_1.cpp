004b1ba19e00ba9a1047b16fd1a2b3b4d02dd6ee
김세은 <153696225+SeEun04@users.noreply.github.com>
2024-10-18 23:45:56 +0900

    Update and rename HW3_1.cpp to HW/hw#3/HW3_1.cpp

--- HW/hw#3/HW3_1.cpp
+++ HW/hw#3/HW3_1.cpp
@@ -36,7 +36,7 @@ Dept::~Dept() {
 }
 
 void Dept::read() {
-    cout << size << "°³ Á¡¼ö ÀÔ·Â>> ";
+    cout << size << "Â°Â³ ÃÂ¡Â¼Ã¶ Ã€Ã”Â·Ã‚>> ";
     for (int i = 0; i < size; ++i) {
         cin >> scores[i];
     }
@@ -52,5 +52,5 @@ int main() {
     Dept com(10); 
     com.read();   
     int n = countPass(com); 
-    cout << "60Á¡ ÀÌ»óÀº " << n << "¸í";
+    cout << "60ÃÂ¡ Ã€ÃŒÂ»Ã³Ã€Âº " << n << "Â¸Ã­";
 }
